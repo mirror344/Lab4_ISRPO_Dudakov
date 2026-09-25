6fe320cb508f019e4402e4d9085de6fcf4ae6161
mirror344 <dudakovivan3@gmail.com>
2026-09-25 21:59:03 +0300

    Added group and data

--- server/Program.cs
+++ server/Program.cs
@@ -1,4 +1,7 @@
 ﻿string name = "Иван";
 string surname = "Дудаков"; 
+string group = "ИСП-241"; 
+string data = "25.09.2026";
+
 
 System.Console.WriteLine("Здравствуйте, это консольное приложение созданное в рамках лабораторной работы №4");
