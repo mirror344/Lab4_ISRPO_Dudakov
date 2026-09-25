f5fa7edc2baaf9123743a4cb88bf2c78e841f806
mirror344 <dudakovivan3@gmail.com>
2026-09-25 22:00:41 +0300

    Added menu

--- server/Program.cs
+++ server/Program.cs
@@ -2,6 +2,14 @@
 string surname = "Дудаков"; 
 string group = "ИСП-241"; 
 string data = "25.09.2026";
+string time = "21:31";
+bool flag = true;
 
 
 System.Console.WriteLine("Здравствуйте, это консольное приложение созданное в рамках лабораторной работы №4");
+while (flag)
+{
+    System.Console.WriteLine("Меню: \n 1 - Показать ФИО \n 2 - Показать группу \n 3 - Показать дату \n 4 - Выход");
+    int choice = int.Parse(Console.ReadLine());
+    
+}
