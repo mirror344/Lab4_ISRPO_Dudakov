54ad15ddbffa96771ab7dfd7520412fac59be3f0
mirror344 <dudakovivan3@gmail.com>
2026-09-25 22:02:00 +0300

    Added logic of choice

--- server/Program.cs
+++ server/Program.cs
@@ -12,4 +12,27 @@ while (flag)
     System.Console.WriteLine("Меню: \n 1 - Показать ФИО \n 2 - Показать группу \n 3 - Показать дату \n 4 - Выход");
     int choice = int.Parse(Console.ReadLine());
     
+    switch (choice) 
+    {
+        case 1:
+            System.Console.WriteLine($"ФИО: {name} {surname}");
+            Console.ReadKey();
+            break;
+        case 2:
+            System.Console.WriteLine($"Группа: {group}");
+            Console.ReadKey();
+            break;
+        case 3:
+            System.Console.WriteLine($"Дата и время: {data}, {time}");
+            Console.ReadKey();
+            break;
+        case 4:
+            System.Console.WriteLine($"Вы вышли из программы");
+            flag = false;
+            break;
+        default:
+            System.Console.WriteLine("Такой команды нет");
+            Console.ReadKey();
+            break;
+    }
 }
